b08ea5317a62af7534b854b53609166cdcc37b3c
Oyebolade Oladokun <oyeboladeoluwaferanmigmail.com>
2022-06-28 00:47:46 +0100

    Initial Commit

---
+++ posts/models.py
@@ -0,0 +1,9 @@
+from inspect import classify_class_attrs
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:50]
